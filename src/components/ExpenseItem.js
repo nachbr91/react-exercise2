ba0db581ba3ebd3b0337aa81c085244bb5ffb40d
nachbr91 <nabero91@gmail.com>
2022-02-04 13:51:06 +0100

    edit expense component adding props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,14 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-  const expenseDate = new Date(2021, 2, 28)
-  const expenseTitle = 'Car Insurance'
-  const expenseAmount = 294.67
+  const { title, amount, date } = props
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toLocaleDateString()}</div>
+      <div>{date.toLocaleDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">$ {amount}</div>
       </div>
     </div>
   )
