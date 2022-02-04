6a42e16178e7abe40e9aebc593f931a8ca5f2449
nachbr91 <nabero91@gmail.com>
2022-02-04 14:03:04 +0100

    change ExpenseItem file to .jsx

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -2,10 +2,17 @@ import './ExpenseItem.css'
 
 function ExpenseItem(props) {
   const { title, amount, date } = props
+  const month = date.toLocaleString('en-US', { month: 'long' })
+  const day = date.toLocaleString('es-US', { day: '2-digit' })
+  const year = date.getFullYear()
 
   return (
     <div className="expense-item">
-      <div>{date.toLocaleDateString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">$ {amount}</div>
