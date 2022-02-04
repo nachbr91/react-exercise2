92699aad87ad727079714305c2584504c737e141
nachbr91 <nabero91@gmail.com>
2022-02-04 19:23:48 +0100

    Edit components

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -1,9 +1,10 @@
 import './ExpenseDate.css'
 
 function ExpenseDate(props) {
-  const month = props.date.toLocaleString('en-US', { month: 'long' })
-  const day = props.date.toLocaleString('es-US', { day: '2-digit' })
-  const year = props.date.getFullYear()
+  const { date } = props
+  const month = date.toLocaleString('en-US', { month: 'long' })
+  const day = date.toLocaleString('es-US', { day: '2-digit' })
+  const year = date.getFullYear()
 
   return (
     <div className="expense-date">
