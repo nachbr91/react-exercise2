5c072c9ca2cc20079be68a9fdae878919966fe29
nachbr91 <nabero91@gmail.com>
2022-02-07 11:49:26 +0100

    Add components folders and change function syntax

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,9 +1,10 @@
 import ExpenseDate from './ExpenseDate'
-import Card from './Card'
+import Card from '../UI/Card'
 import './ExpenseItem.css'
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   const { title, amount, date } = props
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
